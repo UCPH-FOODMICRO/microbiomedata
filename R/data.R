b3c96256d4fb79b4ae07e5b2e2a99b932d6bfc5d
Morten <morten.arendt@dbac.dk>
2021-08-20 10:12:37 +0200

    ud rats

--- R/data.R
+++ R/data.R
@@ -14,3 +14,20 @@
 #' @examples
 #' phyloseq::sample_sums(mice_delivery)
 "mice_delivery"
+
+#' Microbiome of fecal from rats fed with sausage fortified with Inulin
+#'
+#' A phyloseq object containing 16s sequencing of fecal samples from rats feed different diets
+#'
+#' @format A phyloseq object with 480 features and 59 samples
+#' \describe{
+#'   \item{time}{before (Start) or after (slut) dietary intervention}
+#'   \item{Description}{Type of dietary intervention}
+#'   \item{ID}{Rat ID}
+#'   \item{Acetate to Xylose}{Associated Molecular markers}
+#' }
+#' @references Thøgersen, Rebekka, et al. "Ingestion of an Inulin‐Enriched Pork Sausage Product Positively Modulates the Gut Microbiome and Metabolome of Healthy Rats." Molecular nutrition & food research 62.19 (2018): 1800608.
+#' @source \url{https://onlinelibrary.wiley.com/doi/full/10.1002/mnfr.201800608?casa_token=IEsQ-Ry1wQUAAAAA%3AsbD6qevecfM9lUXvc7YnU0SE6sGN4lgHROsjPshSRcoWS0S1mJG060eDZX4yfme8TZJzLNr6MbhhVs0}
+#' @examples
+#' phyloseq::sample_sums(rats_inulin)
+"rats_inulin"
